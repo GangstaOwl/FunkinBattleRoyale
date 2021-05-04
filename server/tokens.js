62ae0766949ee944d8bae679e893b5acf4e5a2c1
GangstaOwl <yasko04@mail.ru>
2021-05-04 14:26:59 +0300

    1
    1

--- server/tokens.js
+++
@@ -1,6 +0,0 @@
-var tokens = {};
-
-tokens.clientToken = 93724324; // This shouldn't be changed.
-tokens.serverToken = 67250746; // This should be changed when the server protocol changes.
-
-module.exports = tokens;
